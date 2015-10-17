95ef8b46d3b6e11f649122c24184e080c7824651
YourMistake <erik.vanlandingham@myemail.indwes.edu>
2015-10-17 11:56:41 -0400

    Official Submission
    Comments and "Nicities" added, check for functionality. It does function
    properly now, only needed a few minor adjustments.

--- RussianPeasant.asm
+++ RussianPeasant.asm
@@ -1,5 +1,7 @@
-; Russian Peasant Algorithm
+; Russian Peasant Algorithm, Ch 7 and Midterm
 ; Erik Vanlandingham
+; CIS-361
+; Last Modified 10-17-15
 
 .386
 .model flat,stdcall
@@ -16,27 +18,28 @@ main proc
     mov eax, numX
     mov ebx, numY
     
-	L1:
-		test eax, eax   ;x==0
-		je finished   
-		test ebx, ebx   ;y==0
-		je finished
+	L1:                         ; I went with a stantard loop from class, it was easier to use
+                                ; Test if Either Value is 0
+                                ; Testing does not modify the registers 
+                                ; whereas the cmp function would 
+		test eax, eax           ; x == 0
+		je finished             ; If a value is equal to 0, we can jump out   
+		test ebx, ebx           ; y == 0
+		je finished             ; If a value is equal to 0, we can jump out   
     
-	    test ebx, 00000001
-		jz Shifts
-		add answer, eax
-		jmp Shifts
-		
-    
+	    test ebx, 00000001      ; Test if the dividing num is odd
+		jz Shifts               ; If it isn't, Jump to the shifts and repeat the loop
+		add answer, eax         ; Otherwise add to the Multiplier
+		jmp Shifts              ; Then shift, and loop
 	 loop L1
     
-	Shifts:
-		shr ebx, 1
-		shl eax, 1
-		jmp L1
+	Shifts:                     ; Creating a jump point for the shifting made life much easier
+		shr ebx, 1              ; Divide Y
+		shl eax, 1              ; Mult X
+		jmp L1                  ; Jump back into checking for 0 values
 
-	finished:
-
+	finished:                   ; Created a Jump Location to easily escape the loop
+                                ; It just allows the program to terminate
 	invoke ExitProcess,0
 main endp
 end main
